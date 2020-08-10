fefc017eb19215736aca07d9dda1c708510fdff4
tao <xdd666@foxmail.com>
2020-08-10 11:18:42 +0800

    demo样式调整完毕

--- Flutter/fish_redux_demo/lib/list/item/view.dart
+++ Flutter/fish_redux_demo/lib/list/item/view.dart
@@ -10,66 +10,82 @@ Widget buildView(ItemState state, Dispatch dispatch, ViewService viewService) {
 Widget _bodyWidget(ItemState state) {
   return Card(
     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+    elevation: 5,
+    margin: EdgeInsets.only(left: 20, right: 20, top: 20),
     child: Row(
       children: <Widget>[
         //左边图片
         Container(
           margin: EdgeInsets.all(20),
           width: 180,
-          height: 350,
-          child: Image.network(state.itemDetail.envelopePic, fit: BoxFit.fill,),
+          height: 100,
+          child: Image.network(
+            state.itemDetail.envelopePic,
+            fit: BoxFit.fill,
+          ),
         ),
         //右边的纵向布局
         Expanded(
             child: Container(
-              margin: EdgeInsets.all(20),
-              height: 350,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  //标题
-                  Expanded(
-                    flex: 1,
-                    child: Text(state.itemDetail.title,
-                      style: TextStyle(fontSize: 34),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
+          margin: EdgeInsets.all(20),
+          height: 150,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              //标题
+              Expanded(
+                flex: 2,
+                child: Container(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    state.itemDetail.title,
+                    style: TextStyle(fontSize: 20),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
                   ),
-                  //内容
-                  Expanded(
-                      flex: 5,
-                      child: Container(
-                        alignment: Alignment.centerLeft,
-                        child: Text(state.itemDetail.desc,
-                          style: TextStyle(fontSize: 28),
-                          maxLines: 4,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      )
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
+                ),
+              ),
+              //内容
+              Expanded(
+                  flex: 5,
+                  child: Container(
+                    alignment: Alignment.centerLeft,
+                    child: Text(state.itemDetail.desc,
+                        style: TextStyle(fontSize: 15),
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis),
+                  )),
+              Expanded(
+                flex: 3,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    //作者
+                    Row(
                       children: <Widget>[
-                        //作者
-                        Row(children: <Widget>[
-                          Text("作者："),
-                          Expanded(child: Text(state.itemDetail.author, style: TextStyle(color: Colors.blue)),)
-                        ],),
-                        //时间
-                        Row(children: <Widget>[
-                          Text("时间："),
-                          Expanded(child: Text(state.itemDetail.niceDate, style: TextStyle(color: Colors.blue)),)
-                        ],),
+                        Text("作者："),
+                        Expanded(
+                          child: Text(state.itemDetail.author,
+                              style: TextStyle(color: Colors.blue),
+                              overflow: TextOverflow.ellipsis),
+                        )
                       ],
                     ),
-                  ),
-                ],
+                    //时间
+                    Row(children: <Widget>[
+                      Text("时间："),
+                      Expanded(
+                        child: Text(state.itemDetail.niceDate,
+                            style: TextStyle(color: Colors.blue),
+                            overflow: TextOverflow.ellipsis),
+                      )
+                    ])
+                  ],
+                ),
               ),
-            )
-        ),
+            ],
+          ),
+        )),
       ],
     ),
   );
