d5cb4a2fbc76a52fe833c6ea5e9e4d7cc66ab95c
yutao <xdd666@foxmail.com>
2020-10-26 22:54:18 +0800

    增加搜索控件

---
+++ Flutter/flutter_use/lib/app/config/images/common_images.dart
@@ -0,0 +1,9 @@
+///公共模块 - 图片资源
+///资源文件变量命名使用小写加下划线：类比系统包Icons的命名
+class CommonImages {
+  ///个人中心模块页面较少,只需要设置一层资源放置目录
+  static const String _common = "assets/images/common/";
+
+  ///搜索框最右边的删除图标
+  static const String search_delete = '${_common}search_delete.png';
+}
