48d43820585e3e9730f20ce24f3cff664e933735
XDD666 <xdd666@foxmail.com>
2021-04-13 13:48:44 +0800

    commit

--- Android/HandlerDemo/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Android/HandlerDemo/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,13 +7,16 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.os.MessageQueue;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView msgTv;
+    private Button bt_fun;
     private Handler mHandler = new Handler(new Handler.Callback() {
         //接收消息,刷新UI
-        @Override public boolean handleMessage(@NonNull Message msg) {
+        @Override
+        public boolean handleMessage(@NonNull Message msg) {
             if (msg.what == 1) {
                 msgTv.setText(msg.obj.toString());
             }
@@ -27,17 +30,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         msgTv = findViewById(R.id.tv_msg);
+        bt_fun = findViewById(R.id.bt_fun);
         //添加IdleHandler实现类
         mHandler.getLooper().getQueue().addIdleHandler(new InfoIdleHandler("我是IdleHandler"));
         mHandler.getLooper().getQueue().addIdleHandler(new InfoIdleHandler("我是大帅比"));
 
         //发送消息
         new Thread(new Runnable() {
-            @Override public void run() {
-                Message message = Message.obtain();
-                message.what = 1;
-                message.obj = "第二种方式 --- 1";
-                mHandler.sendMessageDelayed(message, 100);
+            @Override
+            public void run() {
+                Message msgOne = Message.obtain();
+                msgOne.what = 1;
+                msgOne.obj = "第二种方式 --- 1";
+                msgOne.setAsynchronous(true);
+                mHandler.sendMessage(msgOne);
+
+
+                Message msgThree = Message.obtain();
+                msgThree.what = 1;
+                msgThree.obj = "第二种方式 --- 1111111111111";
+                mHandler.sendMessageDelayed(msgThree, 3000);
             }
         }).start();
     }
